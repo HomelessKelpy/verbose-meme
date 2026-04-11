19197d55ddc1dd44d1adc2f30ac5882c7165072e
Homeless Kelpy <homelesskelpy@gmail.com>
2026-04-12 04:26:17 +0500

    app2 as app3 fixed

--- index3.js
+++ index3.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from App 2 🚀');
+  res.send('Hello from App 3 🚀');
 });
 
 app.listen(PORT, '0.0.0.0', () => {
